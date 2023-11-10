2019fe5d70e7ff3ca523f72c5610d7df5d875f08
LuizBlume <luiz.fernando.251107@gmail.com>
2023-11-10 15:19:56 -0300

    andamento da aula 21

---
+++ projeto_vue/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <router-link>
+        <img src="" alt="" id="logo">
+    </router-link>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+<style >
+</style>
